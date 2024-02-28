393bce40dddc036b0adc731a3c8c771f32e1dbbd
Wasfi Sarrar <61529134+wsarrar@users.noreply.github.com>
2024-02-28 13:11:50 -0700

    Updated files

--- AB_signin.js
+++ AB_signin.js
@@ -2,15 +2,54 @@ const signInForm = document.getElementById("signIn-form");
 const signInButton = document.getElementById("signIn-form-submit");
 const signInErrorMsg = document.getElementById("signIn-error-msg");
 
+const email = document.forms['form']['email'];
+const email_error = document.getElementById('email_error');
+const password = document.forms['form']['password'];
+const password_error = document.getElementById('password_error');
+
+email.addEventListener('userInput', emailVerify);
+password.addEventListener('userInput', passwordVerify);
+
+function signinValid(){
+    if (email.value.length < 11){
+        email.style.border = "1px solid red";
+        email_error.style.display = "block";
+        email.focus();
+        return false;
+    }
+    if(password.value.length < 12){
+        password.style.border = "1px solid red";
+        password_error.style.display = "block";
+        password.focus();
+        return false;
+    }
+}
+
+function emailVerify(){
+    if(email.value.length >= 11){
+        email.style.border = "1px solid green";
+        email_error.style.display = "none";
+        return true;
+    }
+}
+
+function passwordVerify(){
+    if(password.value.length >= 12){
+        email.style.border = "1px solid green";
+        password_error.style.display = "none";
+        return true;
+    }
+}
+
 const forgotPasswordForm = document.getElementById("forgot-password-form");
 const resetPasswordButton = document.getElementById("reset-password-submit");
 
 signInButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const email = signInForm.email.value;
-    const password = signInForm.password.value;
+    const user_email = signInForm.user_email.value;
+    const user_password = signInForm.user_password.value;
 
-    if (email === "user" && password === "web_dev") {
+    if (user_email === "user" && user_password === "web_dev") {
         alert("You have successfully logged in.");
         location.reload();
     } else {
@@ -20,13 +59,13 @@ signInButton.addEventListener("click", (e) => {
 
 resetPasswordButton.addEventListener("click", (e) => {
     e.preventDefault();
-    const email = forgotPasswordForm.email.value;
+    const user_email = forgotPasswordForm.user_email.value;
 
     // Send a password reset email
-    sendPasswordResetEmail(email);
+    sendPasswordResetEmail(user_email);
 });
 
-function sendPasswordResetEmail(email) {
+function sendPasswordResetEmail(user_email) {
     // Implement this function on the server-side
-    alert("A password reset email has been sent to " + email);
+    alert("A password reset email has been sent to " + user_email);
 }

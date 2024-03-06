9bec4df86789f9672643791fa66f27c900f15ad1
Wasfi Sarrar <61529134+wsarrar@users.noreply.github.com>
2024-03-06 16:03:29 -0700

    Reuploaded PHP files.

---
+++ AB_php_get_data.php
@@ -0,0 +1,16 @@
+?php
+if($_SERVER['REQUEST_METHOD'] == "POST){
+
+$username = $_POST['Email'];
+
+$password = $_POST['Password'];
+
+$fName = $_POST['First Name'];
+
+$lName = $_POST['Last Name'];
+
+$bday = $_POST['Birthday'];
+
+$pNum = $_POST['Phone Number'];
+}
+?>

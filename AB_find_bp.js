546455999fb063bbf1aeb950f65132811d92906c
Wasfi Sarrar <61529134+wsarrar@users.noreply.github.com>
2024-03-15 11:44:09 -0700

    Updated files for Build 2

---
+++ AB_find_bp.js
@@ -0,0 +1,27 @@
+const userCardTemplate = document.querySelector("[data-user-template]")
+const userCardContainer = document.querySelector("[data-user-cards-container]")
+const searchInput = document.querySelector("[data-search]")
+
+let users = []
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    users.forEach(user => {
+      const isVisible = user.name.toLowerCase().includes(value) || user.website.toLowerCase().includes(value)
+      user.element.classList.toggle("hide", !isVisible)
+    })
+  })  
+
+fetch("https://jsonplaceholder.typicode.com/users") // TODO: Replace with a link to the data from our DB
+  .then(res => res.json())
+  .then(data => {
+    users = data.map(user => {
+      const card = userCardTemplate.content.cloneNode(true).children[0]
+      const header = card.querySelector("[data-header]")
+      const body = card.querySelector("[data-body]")
+      header.textContent = user.name
+      body.textContent = user.website
+      userCardContainer.append(card)
+      return { name: user.name, website: user.website, element: card }
+    })
+  })
